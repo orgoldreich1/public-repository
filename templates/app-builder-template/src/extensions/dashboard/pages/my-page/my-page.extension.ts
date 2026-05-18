aae06784bfd84a711a25434191e22c49e61bfaca
Or Goldreich <orgold@wix.com>
2026-05-18 10:48:17 +0300

    Add src/extensions/dashboard/pages/my-page/my-page.extension.ts

---
+++ templates/app-builder-template/src/extensions/dashboard/pages/my-page/my-page.extension.ts
@@ -0,0 +1,8 @@
+import { extensions } from '@wix/astro/builders';
+
+export default extensions.dashboardPage({
+  id: '8a7aa320-ca64-47b8-bd5e-d92f23af7da2',
+  title: 'My Page',
+  routePath: '',
+  component: './extensions/dashboard/pages/my-page/my-page.tsx',
+});
